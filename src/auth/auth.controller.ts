72ff81d458e2372737550fce3720c6a0cffacf9b
kyawthihagic <101077682+kyawthihagic@users.noreply.github.com>
2022-12-19 20:56:46 +0630

    feat:docker compose

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,35 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
-import { AccessTokenGuard, RefreshTokenGuard } from './guard';
-import { Request } from 'express';
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+import { AccessTokenGuard, RefreshTokenGuard } from "./guard";
+import { Request } from "express";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+  }
 
-  @Post('login')
+  @Post("login")
   login(@Body() authDto: AuthDto) {
     return this.authService.login(authDto);
   }
 
-  @Post('signup')
+  @Post("signup")
   signup(@Body() authDto: AuthDto) {
     return this.authService.signup(authDto);
   }
 
   @UseGuards(RefreshTokenGuard)
-  @Get('refresh')
+  @Get("refresh")
   refreshTokens(@Req() req: Request) {
-    const userId = req.user['sub'];
-    const refreshToken = req.user['refreshToken'];
+    const userId = req.user["sub"];
+    const refreshToken = req.user["refreshToken"];
     return this.authService.refreshTokens(userId, refreshToken);
   }
 
   @UseGuards(AccessTokenGuard)
-  @Get('logout')
+  @Get("logout")
   logout(@Req() req: Request) {
-    return this.authService.logout(req.user['id']);
+    return this.authService.logout(req.user["id"]);
   }
 }
--- src/auth/guard/access-token.guard.ts
+++ src/auth/guard/access-token.guard.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class AccessTokenGuard extends AuthGuard('jwt') {}
+export class AccessTokenGuard extends AuthGuard("jwt") {
+}
